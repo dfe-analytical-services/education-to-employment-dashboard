b26124ede541142de4c802839fcb330629b15e20
Livia Hull <livia.hull@education.gov.uk>
2022-04-27 15:32:00 +0100

    created table in homepage

--- R/dashboard_text.R
+++ R/dashboard_text.R
@@ -17,9 +17,9 @@ welcome_text <- function() {
       tags$li("Demonstrate how LEO data could contribute to an interactive careers information tool")
     ),
     "For the accompanying statistical release, see: ",
-    a(
-      href = "https://explore-education-statistics.service.gov.uk/find-statistics/"
-      #.noWS = c("after")
+    a(href = "https://explore-education-statistics.service.gov.uk/find-statistics/", 
+      "https://explore-education-statistics.service.gov.uk/find-statistics/",
+      .noWS = c("after")
     ),
     "."
   )
@@ -35,26 +35,24 @@ industry_overview_text <- function() {
     including information on their overall levels of education and average earnings, the 
     sub-sectors of their work, and their subject and qualification choices. 
     It also includes an indicator for whether the selected industry sector is forecast to grow in future years.",
-    br(),
+    br(), br(),
     "Data included on the page are for employees who turn age 25-30 in the 2018-19 tax year. 
     Specifically, the dashboard covers employees born between September 1988 and August 1993 
     who were schooled in England. Employees must be in sustained employment and must not be 
     studying at a higher education institution. 
     For more information, please see the methodology pages in the statistical release:",
-    a(
-      href = "https://explore-education-statistics.service.gov.uk/find-statistics/",
+    a(href = "https://explore-education-statistics.service.gov.uk/find-statistics/", 
+      "https://explore-education-statistics.service.gov.uk/find-statistics/",
       .noWS = c("after")
     ),
     ".", 
-
     tags$h4("Working Futures employment forecasts"),
 
     "The forecast for annual change in employment for each sector and region group 
     between 2022 and 2027 is sourced from: ",
-    a(
-      href = "https://warwick.ac.uk/fac/soc/ier/wf7downloads/"#,
-      #.noWS = c("after")
-    ),
+    a(href = "https://warwick.ac.uk/fac/soc/ier/wf7downloads/",
+      "Working Futures",
+     .noWS = c("after")),
     ".",
     "Working Futures is a quantitative assessment of employment prospects in the UK 
     abour market undertaken by Warwick Institute for Employment Research.",
@@ -63,10 +61,8 @@ industry_overview_text <- function() {
 
     "Education level and subject are based on highest qualification achievement 
     at the start of the 2018-19 tax year. The subject categories used are Sector Subject Area (SSA) tier 1 sourced from: ",
-    a(
-      href = "https://www.gov.uk/government/publications/types-of-regulated-qualifications/qualification-descriptions/"#,
-      #.noWS = c("after")
-    ),
+    a(href = "https://www.gov.uk/government/publications/types-of-regulated-qualifications/qualification-descriptions/",
+      .noWS = c("after")),
     "Level and subject are based on an employee’s full history of education achievements, 
     from qualifications achieved at school and into post-16 and adult education.",
     "The table of top 20 qualifications for each region, sector and level group 
@@ -96,7 +92,7 @@ qualification_pathways_text <- function() {
     all qualifications achieved by employees in post-16 education at level 2 and above.",
     "The pathways are constructed from piecing together popular combinations 
     of qualifications that show progression from:",
-    br(),
+    br(), br(),
     tags$ul(
       tags$li("Level 2 to level 3"),
       tags$li("Level 3 to either level 4/5, level 6 or level 7"),
@@ -123,16 +119,11 @@ sic_groups_text <- function() {
     "The UK Standard Industrial Classification (SIC) of economic activities is used to classify
     businesses by the type of activity they do.",
     h3("Useful links"),
-    a(
-      href = "https://www.gov.uk/government/publications/standard-industrial-classification-of-economic-activities-sic",
-      "Standard industrial classification of economic activities (SIC) - GOV.UK.(www.gov.uk)"#,
-      #.noWS = c("after")
-    ),
+    a(href = "https://www.gov.uk/government/publications/standard-industrial-classification-of-economic-activities-sic",
+      "Standard industrial classification of economic activities (SIC) - GOV.UK.(www.gov.uk)", .noWS = c("after")),
     br(),
-    a(
-      href = "https://onsdigital.github.io/dp-classification-tools/standard-industrial-classification/ONS_SIC_hierarchy_view.html",
-      "ONS interactive SIC hierarchy"#, .noWS = c("after")
-    ),
+    a(href = "https://onsdigital.github.io/dp-classification-tools/standard-industrial-classification/ONS_SIC_hierarchy_view.html",
+      "ONS interactive SIC hierarchy", .noWS = c("after")),
     h3("SIC Groups and sections"),
     "Using the ONS Standard Industrial Classification (SIC) of economic activities, there are over 700 detailed industry codes
     at the five digit level, which are then grouped hierarchically at the four, three and two digit level before being grouped into
@@ -144,28 +135,21 @@ sic_groups_text <- function() {
     of detail to some of the broader sectors.",
     "See below for a description of the sectors included in the dashboard and how they relate to the SIC codes, 
     and see the methodology pages in the statistical release for more detail about the SIC groupings: ",
-    a(
-      href = "https://explore-education-statistics.service.gov.uk/find-statistics/"#, 
-      #.noWS = c("after")
-    ),
-    br(),
+    a(href = "https://explore-education-statistics.service.gov.uk/find-statistics/",
+      "https://explore-education-statistics.service.gov.uk/find-statistics/",
+      .noWS = c("after")),
+    br(), br(), 
     tags$table(border = 0, 
-                         # tags$thead(
-                         #   tags$tr(
-                         #     tags$th(colspan = 2, height = 100, width = 800, 
-                         #             align = "center", "TABLE TITLE")
-                         #   )
-                         # ), 
-                         tags$tbody(
+                         tags$tbody(colspan = 2, height = 50, width = 600,
                            tags$tr(
-                             tags$td(align = "center", strong("Sector")),
+                             tags$td(align = "left", strong("Sector")),
                              tags$td(align = "center", strong("SIC 2007 Section")),
                              tags$td(align = "center", strong("SIC 2007 Division"))
                            ),
                            tags$tr(
                              tags$td(align = "left", 
                                      "Accommodation & food"),
-                             tags$td(align = "left", 
+                             tags$td(align = "center", 
                                      "I"), 
                              tags$td(align = "left", 
                                      "55-56")
@@ -173,80 +157,173 @@ sic_groups_text <- function() {
                            tags$tr(
                              tags$td(align = "left", 
                                      "Agriculture"),
-                             tags$td(align = "left", 
+                             tags$td(align = "center", 
                                      "A"), 
                              tags$td(align = "left", 
                                      "01-03")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Arts & entertainment"),
+                             tags$td(align = "center", 
+                                     "R"), 
+                             tags$td(align = "left", 
+                                     "90-93")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Construction"),
+                             tags$td(align = "center", 
+                                     "F"), 
+                             tags$td(align = "left", 
+                                     "41-43")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Education"),
+                             tags$td(align = "center", 
+                                     "P"), 
+                             tags$td(align = "left", 
+                                     "85")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Electricity & gas"),
+                             tags$td(align = "center", 
+                                     "D"), 
+                             tags$td(align = "left", 
+                                     "35")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Engineering"),
+                             tags$td(align = "center", 
+                                     "C"), 
+                             tags$td(align = "left", 
+                                     "26-28")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Finance & insurance"),
+                             tags$td(align = "center", 
+                                     "K"), 
+                             tags$td(align = "left", 
+                                     "64-66")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Food, drink & tobacco"),
+                             tags$td(align = "center", 
+                                     "C"), 
+                             tags$td(align = "left", 
+                                     "10-12")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Health & social work"),
+                             tags$td(align = "center", 
+                                     "Q"), 
+                             tags$td(align = "left", 
+                                     "86-88")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "IT"),
+                             tags$td(align = "center", 
+                                     "J"), 
+                             tags$td(align = "left", 
+                                     "61-62")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Manufacturing"),
+                             tags$td(align = "center", 
+                                     "C"), 
+                             tags$td(align = "left", 
+                                     "13-25, 29-33")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Media"),
+                             tags$td(align = "center", 
+                                     "J"), 
+                             tags$td(align = "left", 
+                                     "58-60, 63")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Mining & quarrying"),
+                             tags$td(align = "center", 
+                                     "B"), 
+                             tags$td(align = "left", 
+                                     "05-09")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Other services"),
+                             tags$td(align = "center", 
+                                     "S"), 
+                             tags$td(align = "left", 
+                                     "94-96")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Professional services"),
+                             tags$td(align = "center", 
+                                     "M"), 
+                             tags$td(align = "left", 
+                                     "69-75")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Public admin & defence"),
+                             tags$td(align = "center", 
+                                     "O"), 
+                             tags$td(align = "left", 
+                                     "84")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Real estate"),
+                             tags$td(align = "center", 
+                                     "L"), 
+                             tags$td(align = "left", 
+                                     "68")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Support services"),
+                             tags$td(align = "center", 
+                                     "N"), 
+                             tags$td(align = "left", 
+                                     "77-82")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Transport & storage"),
+                             tags$td(align = "center", 
+                                     "H"), 
+                             tags$td(align = "left", 
+                                     "49-53")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Water & sewerage"),
+                             tags$td(align = "center", 
+                                     "E"), 
+                             tags$td(align = "left", 
+                                     "36-39")
+                           ),
+                           tags$tr(
+                             tags$td(align = "left", 
+                                     "Whole & retail trade"),
+                             tags$td(align = "center", 
+                                     "G"), 
+                             tags$td(align = "left", 
+                                     "45-47")
                            )
-                             
-                         #     
-                         #     , 
-                         #             ,
-                         #             "Arts & entertainment", 
-                         #             "Construction", 
-                         #             "Education", 
-                         #             "Electricity & gas", 
-                         #             "Engineering", 
-                         #             "Finance & insurance", 
-                         #             "Food, drink & tobacco", 
-                         #             "Health & social work", 
-                         #             "IT", 
-                         #             "Manufacturing", 
-                         #             "Media",
-                         #             "Mining & quarrying", 
-                         #             "Other services", 
-                         #             "Professional services", 
-                         #             "Public admin & defence", 
-                         #             "Real estate", 
-                         #             "Support services", 
-                         #             "Transport & storage", 
-                         #             "Water & sewerage", 
-                         #             "Whole & retail trade"),
-                         #     tags$td(align = "left", 
-                         #             "I", 
-                         #             "A", 
-                         #             "R", 
-                         #             "F", 
-                         #             "P", 
-                         #             "D",
-                         #             "C",
-                         #             "Q",
-                         #             "J",
-                         #             "C",
-                         #             "J",
-                         #             "B",
-                         #             "S",
-                         #             "M",
-                         #             "O",
-                         #             "L",
-                         #             "N",
-                         #             "H",
-                         #             "E",
-                         #             "G"),
-                         #     tags$td(align = "left", 
-                         #             "55-56",
-                         #             "01-03",
-                         #             "90-93",
-                         #             "41-43",
-                         #             "85",
-                         #             "35",
-                         #             "26-28",
-                         #             "64-66",
-                         #             "10-12",
-                         #             "86-88",
-                         #             "61-62",
-                         #             "13-25, 29-33",
-                         #             "58-60, 63",
-                         #             "05-09",
-                         #             "94-96",
-                         #             "69-75",
-                         #             "84",
-                         #             "68",
-                         #             "77-82",
-                         #             "49-53",
-                         #             "36-39",
-                         #             "45-47")
-                         # )
+                           
+  
     )
     )
 
