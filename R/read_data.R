6ac297e377e66adee7ef829678ee745d3ed0c299
Livia Hull <livia.hull@education.gov.uk>
2022-05-10 08:06:40 +0100

    tidy code

--- R/read_data.R
+++ R/read_data.R
@@ -46,7 +46,7 @@ subsector_v <- stat_subs_sub %>%
   distinct(Subsector, .keep_all = F) %>%
   unlist(use.names = F)
 
-levels_v <- stat_hq_sub %>% 
+levels_v <- stat_hq_sub %>%
   distinct(Level_order, .keep_all = F) %>%
   unlist(use.names = F)
 
@@ -73,30 +73,30 @@ students_in_work <- datalist$students_in_work %>%
 
 # vector for relabel level of qualification
 
-sector_v2 <- qualifications %>% 
+sector_v2 <- qualifications %>%
   distinct(IndustrySector, .keep_all = F) %>%
   unlist(use.names = F)
-  
+
 region_v2 <- qualifications %>%
   distinct(Region, .keep_all = F) %>%
   unlist(use.names = F)
-  
+
 level_v2 <- c("Level 2", "Level 3", "Level 4/5", "Level 6", "Level 7+")
 
 # download data -----------------------------------------------------------
 
 subsectors_table <- datalist$stat_subs %>%
-  select(Region, 
-         Sector,
-         "Sub sector" = Subsector,
+  select(Region,
+    Sector,
+    "Sub sector" = Subsector,
     "Median earnings" = median_income,
     "Proportion" = perc_subs,
     "Volume employees" = number_students_subs
   )
 
 highest_qualification_table <- datalist$stat_hq %>%
-  select(Region, 
-         Sector,
+  select(Region,
+    Sector,
     "Qualification level" = Level_order,
     "Median earnings" = median_income,
     "Proportion" = perc_hq,
@@ -104,13 +104,13 @@ highest_qualification_table <- datalist$stat_hq %>%
   )
 
 qualifications_titles_table <- datalist$stat_hq_sub %>%
-  select(Region, 
-         Sector, 
-         "Sub sector" = Subsector,
-         Qualification,
+  select(Region,
+    Sector,
+    "Sub sector" = Subsector,
+    Qualification,
     "Qualification level" = Level_order_UI,
     "Qualification level in selection filter" = Level_order,
-    "Subject studied" = Subject, 
+    "Subject studied" = Subject,
     "Median earnings" = median_income,
     "Proportion" = perc,
     "Volume employees" = number_students_qual
